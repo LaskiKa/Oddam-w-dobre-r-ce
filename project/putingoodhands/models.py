7244385cb8d9f722f480797633f44f57520f1dd7
LaskiKa <kamil.laski0@gmail.com>
2023-04-15 15:11:47 +0200

    feature-1
    Model Donation added to db
    https://trello.com/c/aNQ7xnKs/27-1-model-donation

--- project/putingoodhands/models.py
+++ project/putingoodhands/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -20,3 +21,16 @@ class Institution(models.Model):
     description = models.TextField()
     type = models.SmallIntegerField(choices=TYPE_CHOICES, default=FUNDATION)
     categories =  models.ManyToManyField(Category)
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution,on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=128)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.CharField(max_length=255)
+    user = models.ForeignKey(User, null=True, default=None, on_delete=models.CASCADE)
