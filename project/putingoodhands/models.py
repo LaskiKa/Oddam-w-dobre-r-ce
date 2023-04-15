81fdbd0ca00dfef9c92717f42b3f2ba8cf9827a0
LaskiKa <kamil.laski0@gmail.com>
2023-04-15 14:41:32 +0200

    Feature-1
    1 Model Institution added to db
    https://trello.com/c/oWsMC7kO/26-1-model-institution

--- project/putingoodhands/models.py
+++ project/putingoodhands/models.py
@@ -4,4 +4,19 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128)
+
+class Institution(models.Model):
     
+    FUNDATION = 1
+    NGO = 2
+    LOCALCHARITY = 3
+    TYPE_CHOICES = [
+        (FUNDATION, 'Fundacja'),
+        (NGO, 'Organizacja pozarządowa'),
+        (LOCALCHARITY, 'Zbiórka lokalna'),
+    ]
+    
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    type = models.SmallIntegerField(choices=TYPE_CHOICES, default=FUNDATION)
+    categories =  models.ManyToManyField(Category)
