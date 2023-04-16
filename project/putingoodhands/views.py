de9d3127a32198a79fd3a26f1d2ff55d7cc599eb
LaskiKa <kamil.laski0@gmail.com>
2023-04-16 09:09:30 +0200

    Feature-2
    Landing Page - Dynamic stats
    number of institution & donation count based on DB

--- project/putingoodhands/views.py
+++ project/putingoodhands/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.views import View
+from putingoodhands.models import Donation, Institution
 
 # Create your views here.
 
 class LandingPageView(View):
     
     def get(self, request):
+        
+        dontaion = Donation.objects.all()
+        institution = Institution.objects.all()
+        
         return render(request,
-                      'index.html')
+                      'index.html',
+                      {'institution': institution,
+                       'dontaion': dontaion})
         
 class LoginView(View):
     
